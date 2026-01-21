63b1d6633ca9d2e6a965b495bf41335f4dd4093b
Dogeng <muhamadky900@gmail.com>
2026-01-21 23:15:09 +0700

    Update main.py

--- rangkuman/main.py
+++ rangkuman/main.py
@@ -1,57 +1,18 @@
-data_base = {}
+import keamanan as aman
 flag = False
 
-#membuat sistem daftar
-def sistem_daftar():
-    nama_user = input("masukan nama: ").lower()
-    password_user = input("masukan password: ").lower()
-    #cek apakah user pernah daftar
-    if nama_user in data_base:
-        print("maaf kamu sudah terdaftar")
+while 0 < 1:
+    print("ketik (mulai) untuk menjalakan")
+    mulai = input(" "*10).lower()
+    if mulai == "mulai":
+        flag = True
+        print("="*20,"selamat datang!","="*20)
+        print("="*10,"ketik (1) daftar (2) login (3) exsit","="*10)
+        break
     else:
-        data_base[nama_user] = password_user
-        print(f"pendaftaran berhasil")
-
-#membuat sistem login
-def sistem_login():
-    nama_user = input("masukan nama: ").lower()
-    password_user = input("masukan password: ").lower()
-    #megambil key di database
-    key = data_base.get(nama_user)
+        print("ketik yang benar")
     
-    if password_user == key:
-        print("login berhasil")
-        return True
-    else:
-        print("login gagal")
-        return False
-#sistem_login()
-
-#membuat sistem keamana
-def sistem_keamanan():
-    nomor = 0
-    #loping keamanan
-    while nomor < 5:
-            nomor += 1
-            user = sistem_login()
-            
-            if user == True:
-                print("akses di terima")
-                return True
-            if nomor > 3:
-                print("ada akses yang mencurigakan")
-                return False
-
-print("ketik (mulai) untuk menjalakan")
-mulai = input(" "*10).lower()
-
-if mulai == "mulai":
-    flag = True
-    print("="*20,"selamat datang!","="*20)
-    print("="*10,"ketik (1) daftar (2) login (3) exsit","="*10)
-else:
-    print("ketik yang benar")
-
+menu = ["daftar akun", "login akun"]
 #logika Utama
 login = False
 while flag:
@@ -60,19 +21,18 @@ while flag:
     
     if input_user == "1":
         print("selama datang di pendaftaran")
-        daftar = sistem_daftar()
+        daftar = aman.sistem_daftar()
         
     elif input_user == "2":
         print("login akun kamu untuk masuk")
-        login = sistem_keamanan()
+        login = aman.sistem_keamanan()
         
         if login == True:
             print("menu")
             print(login)
         elif login == False:
             flag = False
-            print(login)
-        else:
+            print(login
             print("gagal masuk")
             
     elif input_user == "3":
