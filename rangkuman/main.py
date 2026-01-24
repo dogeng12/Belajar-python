3d65e62f66cc8f8c57e9a61d613d488ca5f81aed
Dogeng <muhamadky900@gmail.com>
2026-01-24 21:14:01 +0700

    Update main.py
    memperbaiki kode

--- rangkuman/main.py
+++ rangkuman/main.py
@@ -1,41 +1,100 @@
-import keamanan as aman
-flag = False
+import json
+import os 
 
-while 0 < 1:
-    print("ketik (mulai) untuk menjalakan")
-    mulai = input(" "*10).lower()
-    if mulai == "mulai":
-        flag = True
-        print("="*20,"selamat datang!","="*20)
-        print("="*10,"ketik (1) daftar (2) login (3) exsit","="*10)
-        break
+#membuat sistem data base
+data_base = "data_base.json"
+
+#fungsi cek data di file
+def ambil_data():
+    if not os.path.exists(data_base):
+        return {}
+    try:
+        with open(data_base, "r") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        print("data belom ada di file")
+        return {}
+
+#fungsi simpan data di file
+def simpan_data(simpan):
+    with open(data_base, "w") as file:
+        json.dump(simpan, file, indent=4)
+
+#menambah data awal di file
+data_awal = ambil_data()
+data_awal = {"id_user": {"profil": {"username": "rizky", "password": "123","menu_makanan": ["ayam goreng", "nasi goreng", "es teh"]
+}}}
+simpan_data(data_awal) #data disimpan
+
+#membuat sistem daftar dan login
+
+#memangil funsi data base
+data_daftar_login = ambil_data()
+
+#membuat sistem daftar
+def sistem_daftar():
+    data_daftar = ambil_data()
+    
+    nama_user = input("masukan nama: ").lower()
+    password_user = input("masukan password: ").lower()
+    
+    #cek apakah user pernah daftar
+    user_udah_ada = False
+    for id_user in data_daftar:
+        if data_daftar["id_user"]["profil"]["username"] == nama_user:
+            user_udah_ada = True
+            break
+    if user_udah_ada:
+        print("nama kamu sudah ada")
     else:
-        print("ketik yang benar")
+        #menambah key baru di data base
+        add_id = f"id_user{len(data_daftar) + 1}"
+        data_daftar[add_id] = {"profil": {"username": nama_user, "password": password_user,"menu_makanan": ["ayam goreng", "nasi goreng", "es teh"]
+}}
+        simpan_data(data_daftar)
+        print("kamu sudah terdaftar")
+         #fungsi ini berakhir disini!
+         
+
+#membuat sistem login
+def sistem_login():
+    data_login = ambil_data()
     
-menu = ["daftar akun", "login akun"]
-#logika Utama
-login = False
-while flag:
-    input_user = input().lower()
+    nama_user = input("masukan nama: ").lower()
+    password_user = input("masukan password: ").lower()
+    #megambil key di database
+    key = data_login.get("password")
     
-    
-    if input_user == "1":
-        print("selama datang di pendaftaran")
-        daftar = aman.sistem_daftar()
-        
-    elif input_user == "2":
-        print("login akun kamu untuk masuk")
-        login = aman.sistem_keamanan()
-        
-        if login == True:
-            print("menu")
-            print(login)
-        elif login == False:
-            flag = False
-            print(login
-            print("gagal masuk")
+    if password_user == key:
+        print("login berhasil")
+        print(key)
+        return True
+    else:
+        print("login gagal")
+        print(key)
+        return False
+cek = sistem_login()
+print(cek)
+
+#membuat sistem keamana
+def sistem_keamanan():
+    nomor = 3
+    #loping keamnan
+    while nomor > 0:
+            nomor -= 1
+            user = sistem_login()
             
-    elif input_user == "3":
-        break
-    else:
-        print("ada yang salah")
+            if user == True:
+                print("akses di terima")
+                return True
+            else:
+                print(f"kesempata login kamu cuma (3) sisa {nomor}")
+                
+   #kasih nilai False jika user salah input 3 x             
+    print("ada akses yang mencurigakan")
+    return False
+#cek = sistem_keamanan()
+#print(cek)    
+#membuat data base
+#membuat fitur login
+#membuat restoran makanan
